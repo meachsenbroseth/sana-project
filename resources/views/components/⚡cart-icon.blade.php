9b422a11e1f03571f08f3fe2a5bfa5e16fb24452
meachsenbroseth <153406625+meachsenbroseth@users.noreply.github.com>
2026-02-09 22:13:49 +0700

    Properly listens to cart-updated events

--- resources/views/components/⚡cart-icon.blade.php
+++ resources/views/components/⚡cart-icon.blade.php
@@ -1,6 +1,7 @@
 <?php
 
 use Livewire\Component;
+use Livewire\Attributes\On;
 
 new class extends Component {
     public int $cartCount = 0;
@@ -16,12 +17,14 @@ new class extends Component {
         $cart = session()->get('cart', []);
 
         $this->cartCount = collect($cart)->sum('quantity');
+
+        $this->dispatch('cart-updated');
     }
 };
 ?>
 
 <div>
-    <a href="{{route('cart.index')}}" class="relative">
+    <a href="{{ route('cart.index') }}" wire:navigate class="relative">
         <svg class="h-5 w-5 text-gray-700" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
             stroke-linejoin="round">
@@ -33,7 +36,7 @@ new class extends Component {
             <span
                 class="absolute -top-2 -right-2 bg-red-600 text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
                 {{ $cartCount }}
-            </span>     
+            </span>
         @endif
     </a>
 </div>
