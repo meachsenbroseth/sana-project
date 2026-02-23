0cad45c180adb6178924dcc4c3ea6e9bf382f01a
meachsenbroseth <153406625+meachsenbroseth@users.noreply.github.com>
2026-02-23 09:52:30 +0700

    fix address in checjout page

--- resources/views/pages/⚡checkout.blade.php
+++ resources/views/pages/⚡checkout.blade.php
@@ -545,7 +545,7 @@ new class extends Component {
 
                                 <div class="grid grid-cols-2 gap-4">
                                     <div>
-                                        <label class="block text-sm font-medium text-gray-700 mb-2">City *</label>
+                                        <label class="block text-sm font-medium text-gray-700 mb-2">City/Province *</label>
                                         <input type="text" wire:model="city"
                                             class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500">
                                         @error('city')
@@ -554,7 +554,7 @@ new class extends Component {
                                     </div>
                                     <div>
                                         <label
-                                            class="block text-sm font-medium text-gray-700 mb-2">State/Province</label>
+                                            class="block text-sm font-medium text-gray-700 mb-2">District/Khan</label>
                                         <input type="text" wire:model="state"
                                             class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500">
                                     </div>
